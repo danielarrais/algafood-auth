b89c9e546893629ed3e60244c3243905a92fabbe
danielarrais <danielarraiscarvalho@gmail.com>
2021-02-21 03:26:32 -0300

    Configura refresh token

--- src/main/java/com/danielarrais/algafood/auth/conf/AuthorizationServerConfig.java
+++ src/main/java/com/danielarrais/algafood/auth/conf/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package com.danielarrais.algafood.auth.conf;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -17,6 +18,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     private AuthenticationManager authenticationManager;
 
     @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
     @Override
@@ -24,10 +28,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         clients.inMemory()
                 .withClient("algafood-api")
                 .secret(passwordEncoder.encode("web123"))
-                .authorizedGrantTypes("password")
+                .authorizedGrantTypes("password", "refresh_token")
                 .scopes("write", "read")
                 .accessTokenValiditySeconds(60 * 60 * 6)
-                .and()
+            .and()
                 .withClient("refresh-token")
                 .secret(passwordEncoder.encode("web123"));
 
@@ -40,6 +44,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-        endpoints.authenticationManager(authenticationManager);
+        endpoints
+                .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService);
     }
 }
